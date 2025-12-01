e4ce957df13354039ac40b42c5811a3c32734180
Thananon PROMPAK <phupha20517@gmail.com>
2025-12-01 21:07:18 +0000

    File changes

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -3232,17 +3232,28 @@ export default function Settings() {
                             คลิกปุ่มด้านล่างเพื่อเชื่อมต่อกับ Facebook Page ของคุณแบบอัตโนมัติ
                           </p>
                           
-                          <div 
-                            dangerouslySetInnerHTML={{
-                              __html: `
-                                <div id="fb-root"></div>
-                                <fb:login-button 
-                                  scope="pages_show_list,pages_messaging,pages_read_engagement,pages_manage_metadata"
-                                  onlogin="checkLoginState();">
-                                </fb:login-button>
-                              `
+                          <Button
+                            type="button"
+                            onClick={() => {
+                              if (window.FB) {
+                                window.FB.login(function(response) {
+                                  if (response.authResponse) {
+                                    window.checkLoginState();
+                                  } else {
+                                    console.log('User cancelled login or did not fully authorize.');
+                                  }
+                                }, {scope: 'pages_show_list,pages_messaging,pages_read_engagement,pages_manage_metadata'});
+                              } else {
+                                toast.error('Facebook SDK ยังไม่พร้อม กรุณารอสักครู่');
+                              }
                             }}
-                          />
+                            className="w-full bg-[#1877f2] hover:bg-[#166fe5] text-white text-base font-semibold py-6"
+                          >
+                            <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24" fill="currentColor">
+                              <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
+                            </svg>
+                            เชื่อมต่อกับ Facebook
+                          </Button>
                         </div>
 
                         {/* หรือ */}
